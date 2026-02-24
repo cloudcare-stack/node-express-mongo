eef6f1f56097fb8ee1e3b514112c735b84f4f995
Colin McAteer <co8mcate@gmail.com>
2026-02-23 18:31:37 -0800

    Tweaked a bit with bootstrap

--- chapter-5-mongodb/src/app.js
+++ chapter-5-mongodb/src/app.js
@@ -29,9 +29,8 @@ app.use('/', pagesRouter);
 app.use('/', postsRouter);
 
 // 404 handler
-app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, '..', 'views', '404.html'));
+app.use((req, res) => {
+  res.status(404).render("404", { title: "Not Found" });
 });
-
 // Export the app for use in the server.js file
 module.exports = app;
